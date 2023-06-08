12a1a5d79de0d79fc7ef3301bffe8a67e10e3131
Raymundo Paz <rayadoxav@hotmail.com>
2023-06-08 04:22:02 -0600

    Cors test 5

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ function start(): void {
   server.app.use(express.json());
 
   server.app.use(cors({ origin: true, credentials: true, methods: ['GET', 'POST', 'DELETE', 'PUT'] }));
-  server.app.options('*', cors());
+  server.app.options('*', cors({ origin: true, credentials: true, methods: ['GET', 'POST', 'DELETE', 'PUT'] }));
 
   server.app.use((error: any, _: Request, response: Response, next: NextFunction) => {
     //@ts-ignore
